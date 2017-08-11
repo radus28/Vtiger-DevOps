72c54b517df718461418c9584450671df2c86494
Saujanyan <ssaujanyan@gmail.com>
2017-08-11 15:54:01 +0530

    Task Completed

--- fields/ui56.php
+++ fields/ui56.php
@@ -1,12 +1,10 @@
 <?php
-
 $Vtiger_Utils_Log = true;
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', '1');
 include_once('../../config.inc.php');
 set_include_path($root_directory);
 include_once('vtlib/Vtiger/Module.php');
-include_once('vtlib/Vtiger/Block.php');
 
 //Module Nmae
 $moduleInstance = Vtiger_Module::getInstance('Contacts');
@@ -14,22 +12,18 @@ $moduleInstance = Vtiger_Module::getInstance('Contacts');
 $blockInstance = Vtiger_Block::getInstance('LBL_CONTACT_INFORMATION',$moduleInstance);
 
 
-$fieldInstance = Vtiger_Field::getInstance("picklist_test", $moduleInstance);
+$fieldInstance = Vtiger_Field::getInstance("payment_made_test", $moduleInstance);
         if ($fieldInstance === false) {
 $fieldInstance = new Vtiger_Field();
-$fieldInstance->name = 'picklist_test';
-$fieldInstance->label = 'LBL_APPLICATION_STATUS';
+$fieldInstance->name = 'payment_made_test';
+$fieldInstance->label = 'LBL_PAYMENT_MADE_TEST';
 $fieldInstance->table = 'vtiger_contactdetails';
-$fieldInstance->column = 'picklist_test';
-$fieldInstance->columntype = 'varchar(100)';
-$fieldInstance->uitype = 15;
-$fieldInstance->setPicklistValues(array('test1','test2'));
-$fieldInstance->typeofdata = 'V~O';
+$fieldInstance->column = 'payment_made_test';
+$fieldInstance->columntype = 'varchar(3)';
+$fieldInstance->uitype = 56;
+$fieldInstance->typeofdata = 'C~O';
 $blockInstance->addField($fieldInstance);
 }
-
-
-
 
 
 echo 'fields done';
