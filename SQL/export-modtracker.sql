7ae07a6e0618f3a7d414a0e22ef12d3b7e3e3dc3
Radus28 Software Solutions <ajstharsan@gmail.com>
2019-11-01 13:39:04 +0530

    Create export-modtracker.sql

---
+++ SQL/export-modtracker.sql
@@ -0,0 +1,9 @@
+SELECT mb.id,mb.crmid,mb.module, mb.changedon, md.fieldname, md.prevalue AS last_value,md.postvalue AS new_value,
+ u.user_name,CONCAT (u.first_name, " ",u.last_name) AS Changedby,
+ce.label,   
+mr.targetmodule AS relatedmodule,mr.targetid AS related_crmid
+FROM `vtiger_modtracker_basic` mb 
+INNER JOIN vtiger_crmentity ce ON ce.crmid = mb.crmid 
+LEFT JOIN vtiger_users u ON u.id = mb.whodid
+LEFT JOIN vtiger_modtracker_relations mr ON mb.id = mr.id 
+LEFT JOIN vtiger_modtracker_detail md ON mb.id = md.id 
