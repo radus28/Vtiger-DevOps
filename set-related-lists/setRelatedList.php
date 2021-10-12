754ac1390767eb717931cef9e00a073502cd2f17
Radus28 Software Solutions <ajstharsan@gmail.com>
2021-10-12 11:45:10 +0530

    Update setRelatedList.php
    Comments added

--- set-related-lists/setRelatedList.php
+++ set-related-lists/setRelatedList.php
@@ -8,11 +8,17 @@ set_include_path($root_directory);
 include_once('vtlib/Vtiger/Module.php');
 include_once('vtlib/Vtiger/Block.php');
 //
-$module = filter_input(INPUT_GET, 'module');//  URL?module=<modulename>
-$childModule = filter_input(INPUT_GET, 'childModule');//  URL?childmodule=<modulename>
-var_dump($module);
-//if ($module!='' && $childModule!=''){
+$module = filter_input(INPUT_GET, 'module');//  URL?module=<modulename> , parent module
+$childModule = filter_input(INPUT_GET, 'childModule');//  URL?childmodule=<modulename>, child module
+if ($module!='' && $childModule!=''){
     $moduleOb = Vtiger_Module::getInstance($module);
     $moduleChildOb = Vtiger_Module::getInstance($childModule);
+    /**
+    * Use any of these to enable buttons in related list : array('ADD') | array('SELECT') | array('ADD','SELECT)
+    * Instead of 'get_dependents_list':
+    * Use get_related_list for 1:M Relation, eg, Project & project tasks. This will create a reference field of parent module into child module
+    * Use get_activity_list if child module is calendar
+    * User get_emails if child module is Emails
+    **/
     var_dump($moduleOb->setRelatedList($moduleChildOb, $childModule, array('ADD'), 'get_dependents_list'));
-//}
+}
