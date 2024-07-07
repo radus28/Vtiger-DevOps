588932f6d28f363e0437c1c8edf2630638c08468
Radus28 Software Solutions <ajstharsan@gmail.com>
2024-07-07 23:36:38 +0530

    Create create-sample.php
    Create module from console using php

---
+++ modules/create-sample.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('error_reporting', E_ALL & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
+ini_set('display_errors', '1');
+include_once ('../../config.inc.php');
+global $root_directory, $adb;
+set_include_path($root_directory); //for include root path
+include_once ('vtlib/tools/console.php');
+
+$moduleInformation = [
+    'name' => 'Trialmodule',
+    'parent' => 'SALES',
+    'entityfieldname' => 'tm_name',
+    'entityfieldlabel' => 'TM Name',
+
+];
+class Radus_ModuleController extends Vtiger_Tools_Console_ModuleController
+{
+    public static function build($moduleInformation)
+    {
+        $createModuleController = new Vtiger_Tools_Console_ModuleController();
+        $createModuleController->create($moduleInformation);
+        echo 'Module created successfully';
+    }
+}
+$moduleController = Radus_ModuleController::build($moduleInformation);
+
